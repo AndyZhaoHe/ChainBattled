3ca09b8a6a5ff80d9c159b0a236959c6a328190f
qingchen <206888978@qq.com>
2022-08-16 09:50:05 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,18 +2,15 @@ require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 
-
 module.exports = {
   solidity: "0.8.10",
   networks: {
-    mumbai:{
+    mumbai: {
       url: process.env.TESTNET_RPC,
       accounts: [process.env.PRIVATE_KEY]
     },
   },
-  etherscan:{
-    apiKey: {
-      polygonMumbai: process.env.POLYGONSCAN_API_KEY
-    }
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
   }
 };
